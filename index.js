0ddbb2f14a77062215a4e7846c7f12edd22a8a2d
themdshakibul <mdshakibul88240@gmail.com>
2026-06-25 21:15:57 +0600

    add: vercel json change

--- index.js
+++ index.js
@@ -522,9 +522,9 @@ async function run() {
       res.json({ success: true, result });
     });
 
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!",
-    // );
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // await client.close();
   }
